68c8701eeb1061f0787d2d7e40416bbe9c587416
Luis Peraza <luishdz9787@gmail.com>
2020-01-23 10:12:22 -0600

    Hola mundo

--- PruebaGit/src/main/java/PruebaGit/Main.java
+++ PruebaGit/src/main/java/PruebaGit/Main.java
@@ -4,18 +4,16 @@
  * and open the template in the editor.
  */
 package PruebaGit;
+import java.text.*;
+import java.lang.*;
 
-/**
- *
- * @author Mario
- */
-public class Main {
+class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("Hola mundo");
     }
     
 }
